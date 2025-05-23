dd92801c67221cfbf0b2b26e21cc9f40485ae0f3
Saddam Khoso <Saddamkhoso203@gmail.com>
2025-05-23 09:21:49 +0500

    after login move to admin, login testing

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -8,15 +8,16 @@
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
  <div class="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
      <h1 class="text-2xl  mb-6 text-center text-gray-800 " >admin login</h1>  
-     <form action="" method="post" class="space-y-4">
+     <form action="/admin-login" method="post" class="space-y-4">
+        @csrf
         <div>
             <label for="" class="text-gray-600 mb-1 "> Admin Name</label>
-            <input type="text" placeholder="Enter Admin Name"
+            <input type="text" placeholder="Enter Admin Name" name="name"
             class="w-full px-4 py-2 border border-gray-300 rounded-xl  focus:outline-none ">
         </div>
              <div>
             <label for="" class="text-gray-600 mb-1 "> Password</label>
-            <input type="text" placeholder="Enter Admin Password"
+            <input type="password" placeholder="Enter Admin Password" name="password"
             class="w-full px-4 py-2 border border-gray-300 rounded-xl  focus:outline-none ">
         </div>
         <button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white">Login</button>
